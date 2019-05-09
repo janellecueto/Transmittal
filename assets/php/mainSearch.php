c77e60fb5a4ad9546110123daec28a8734e19464
Janelle Cueto <janellecueto@outlook.com>
2019-05-09 11:52:15 -0500

    updated

--- assets/php/mainSearch.php
+++ assets/php/mainSearch.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("../../info.php");
+
 $startDate = "";
 if(array_key_exists('startDate', $_POST)) $startDate = $_POST['startDate'];
 $endDate = "";
@@ -14,3 +16,28 @@ $invoiceNo = "";
 if(array_key_exists('invoiceNo', $_POST)) $invoiceNo = $_POST['invoiceNo'];
 
 $type = $_POST['type'];
+
+$conn = new mysqli($host, $user, $password, $defaultTbl);
+if($conn->errno){
+    print "<br>Error: ".$conn->error;
+    exit();
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo "ya made it to this page! woo!<br>";
+echo "$startDate";
+    ?>
+</body>
+</html>
