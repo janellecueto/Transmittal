37579c7bd9b13c04c25dddd18363c50b8fdac9a4
Janelle Cueto <janellecueto@outlook.com>
2019-05-09 16:15:24 -0500

    updated

--- assets/php/mainSearch.php
+++ assets/php/mainSearch.php
@@ -1,22 +1,22 @@
 <?php
 
-require_once("../../info.php");
+//require_once("../../info.php");
 
-$startDate = "";
-if(array_key_exists('startDate', $_POST)) $startDate = $_POST['startDate'];
-$endDate = "";
-if(array_key_exists('endDate', $_POST)) $endDate = $_POST['endDate'];
-$jobNumber = "";
-if(array_key_exists('jobNumber', $_POST)) $jobNumber = $_POST['jobNumber'];
-$clientCode = "";
-if(array_key_exists('clientCode', $_POST)) $clientCode = $_POST['clientCode'];
-$signature = "";
-if(array_key_exists('signature', $_POST)) $signature = $_POST['signature'];
-$invoiceNo = "";
-if(array_key_exists('invoiceNo', $_POST)) $invoiceNo = $_POST['invoiceNo'];
-
+//$startDate = "";
+//if(array_key_exists('startDate', $_POST)) $startDate = $_POST['startDate'];
+//$endDate = "";
+//if(array_key_exists('endDate', $_POST)) $endDate = $_POST['endDate'];
+//$jobNumber = "";
+//if(array_key_exists('jobNumber', $_POST)) $jobNumber = $_POST['jobNumber'];
+//$clientCode = "";
+//if(array_key_exists('clientCode', $_POST)) $clientCode = $_POST['clientCode'];
+//$signature = "";
+//if(array_key_exists('signature', $_POST)) $signature = $_POST['signature'];
+//$invoiceNo = "";
+//if(array_key_exists('invoiceNo', $_POST)) $invoiceNo = $_POST['invoiceNo'];
+//
 $type = "";
-if(array_key_exists('type', $_POST)) $type = $_POST['type'];
+//if(array_key_exists('type', $_POST)) $type = $_POST['type'];
 
 $firstCol = "Serialno";
 if($type === 3)
