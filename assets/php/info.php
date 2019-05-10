026f34913642c4fcf48b25d81743ff5209a3187b
Janelle Cueto <janellecueto@outlook.com>
2019-05-10 11:06:10 -0500

    updated

---
+++ assets/php/info.php
@@ -0,0 +1,14 @@
+<?php
+$host = "SERVER2008";
+$user = "TC";
+$password = "Dickerson1";
+$defaultTbl = "tc";
+
+$transTbl = "trans";
+$transTblOld = "trans91_18";
+$faxTbl = "faxtr";
+$faxTblOld = "faxtr94_17";
+$pbillTbl = "pbill";
+$pbillTblOld = "pbill01_18";
+
+//echo "hello?";
