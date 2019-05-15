b199e60275938c508d08b5925257b2340dec00cd
Janelle Cueto <janellecueto@outlook.com>
2019-05-15 15:19:08 -0500

    updated billplot

--- billplot/plottingPrices.php
+++ billplot/plottingPrices.php
@@ -1,9 +1,6 @@
 <?php
 /**
  * Displays or returns plotting prices
- *
- * NOTE: the number of sizes is hardcoded (9). This may come up in the future wherein you will
- *       have to update ./index for sheet size option and what not
  */
 
 include("../assets/php/info.php");
@@ -19,14 +16,52 @@ if(array_key_exists("ret", $_GET)) $ret = $_GET['ret'];
 
 $query = "SELECT * FROM tc.mcosts_test";
 $result = $conn->query($query);
-$row = $result->fetch_all();        //fetch all returns all rows from table. there should only be 9
+$rows = $result->fetch_all();        //fetch all returns all rows from table. there should only be 18 for ss1 - ss9
 
 if($ret){
-    echo json_encode($row);
+    echo json_encode($rows);
     exit;
 }
 $conn->close();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="../assets/css/mainStyle.css">
+    <title>Current Plotting Prices</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table table-sm">
+            <thead class="sticky-top">
+                <th scope="col">Size</th>
+                <th scope="col">Media</th>
+                <th scope="col">Cost - Color</th>
+                <th scope="col">Cost - No color</th>
+            </thead>          
+            <tbody>
+<?php
+            $len = sizeof($rows);
+            for($i = 0; $i<$len-1; $i+=2){       //this table is organized every other row x_______x don't know why 
+                echo "<tr>";
+                echo "<td>".$rows[$i][0]."</td>";
+                echo "<td>".$rows[$i][1]."</td>";
+                echo "<td>".$rows[$i][3]."</td>";
+                echo "<td>".$rows[$i+1][3]."</td>";
+                echo "</tr>";
+            }
+?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
 
 
