104be555fa32c7ddae5c38c05b0b7181ee52201b
Janelle Cueto <janellecueto@outlook.com>
2019-05-29 16:14:42 -0500

    updated

--- fax/print.php
+++ fax/print.php
@@ -1,4 +1,13 @@
 <?php
+
+require_once("../assets/php/info.php");
+include("../assets/php/current.php");
+include_once("../assets/createPDF.php");
+include_once("../label-envelope/printLabel.php");
+include_once("../label-envelope/printEnvelope.php");
+
+$debug = true;
+
 $jobNumber = $_POST['jobNumber'];
 $clientCode = $_POST['clientCode'];
 $date = new DateTime($_POST['date']);
@@ -19,17 +28,72 @@ $extraComp = $_POST['extraComp'];
 $extraName = $_POST['extraName'];
 $extraFax = $_POST['extraFax'];
 
-echo "hello!<br>";
-echo "jobNumber: $jobNumber<br>";
-echo "clientCode: $clientCode<br>";
-echo "date: ".$date->format("Y-m-d")."<br>";
-echo "attention: $attention<br>";
-echo "company: $company<br>";
-echo "fax: $fax<br>";
-echo "from: $from<br>";
-echo "numPages: $numPages<br>";
-echo "willFollow: $willFollow<br>";
-echo "remarks: $remarks<br>";
-echo "extraComp: ".implode(", ", $extraComp)."<br>";
-echo "extraName: ".implode(", ", $extraName)."<br>";
-echo "extraFax: ".implode(", ", $extraFax)."<br>";
+$save = $_POST['save'];
+
+// echo "hello!<br>";
+// echo "jobNumber: $jobNumber<br>";
+// echo "clientCode: $clientCode<br>";
+// echo "date: ".$date->format("Y-m-d")."<br>";
+// echo "attention: $attention<br>";
+// echo "company: $company<br>";
+// echo "fax: $fax<br>";
+// echo "from: $from<br>";
+// echo "numPages: $numPages<br>";
+// echo "willFollow: $willFollow<br>";
+// echo "remarks: $remarks<br>";
+// echo "extraComp: ".implode(", ", $extraComp)."<br>";
+// echo "extraName: ".implode(", ", $extraName)."<br>";
+// echo "extraFax: ".implode(", ", $extraFax)."<br>";
+$conn = new mysqli($host, $user, $password, $defaultTbl);
+if($conn->errno){
+    echo "Error: ".$conn->error();
+    exit();
+}
+
+if(intval($save)){
+    $mainQuery = "INSERT INTO tc.$faxTbl (`Date`, Code, Company, Jn, Project, Attention, FaxNumber, NumberPages, ";
+    $values = " VALUES('$date', '$clientCode', '$company', '$jobNumber', '$project', '$attention', '$fax', $numPages, ";
+    if($remarks){
+        $mainQuery .= "Remarks, ";
+        $values .= "'$remarks',";
+    }
+    $mainQuery .= "Signed";
+    $values .= "'$from'";
+    $mainQuery .= ", WillFollow";
+    if($willFollow){
+        $values .= ','.true;
+    }
+    else{
+        $values .= ', 0';
+    }
+    for($i=1; $i<= 2; $i++){
+        if($extraComp[$i-1]){
+            $mainQuery .= ", Copy_".$i."_Who";
+            $values .= ', "'.$extraName[$i-1].'"';
+            $mainQuery .= ", Copy_".$i."_Co";
+            $values .= ', "'.$extraComp[$i-1].'"';
+            $mainQuery .= ", Copy_".$i."_Fax";
+            $values .= ', "'.$extraFax[$i-1].'"';
+        }
+    }
+    $mainQuery .= ')';
+    $values .= ')';
+    $mainQuery .= $values;
+
+    //echo $mainQuery;
+    if(!$debug){
+        if($conn->query($mainQuery) === true){
+            echo "New record created successfully in fax_test";
+        }
+        else{
+            echo "<br>ERROR: ".$conn->error;
+        }
+    }
+    else{
+        echo $mainQuery."<br>";
+    }
+}
+
+//$serialResult->free();
+// $serial = findCurrentNo($conn, $sfQuery);
+mysqli_close($conn);
